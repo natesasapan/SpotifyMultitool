0367d13facd0879e2b0cfbe654b05f7ca28d3596
Nate Sasapan <54950993+natesasapan@users.noreply.github.com>
2024-02-05 08:36:10 -0500

    Add files via upload
    First Commit

---
+++ spotify_multitool.py
@@ -0,0 +1,70 @@
+import os
+from dotenv import load_dotenv
+import base64
+from requests import post,get
+import json
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+redirect_uri = os.getenv("URI_REDIR")
+
+conf = (client_id, client_secret, redirect_uri)
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers,data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists...")
+        return None
+    
+    return json_result[0]
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url,headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
+def get_user_playlists(token, user_id):
+    url = f"https://api.spotify.com/v1/users/{user_id}/playlists?limit=1&offset=0"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["items"]
+    return json_result
+
+def get_songs_from_playlist(token, playlist_id):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks??market=US&fields=fields%3Ditems%28added_by.id%2Ctrack%28name%2Chref%2Calbum%28name%2Chref%29%29%29&limit=5&offset=0"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["items"]
+    return json_result
+
+token = get_token()
