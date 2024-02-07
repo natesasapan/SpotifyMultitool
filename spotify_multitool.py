899b6511b90ead94474ff0042734541ab1943f1d
Nate Sasapan <natesasapan@gmail.com>
2024-02-07 08:49:43 -0500

    Added functionality to print all songs in a Spotify playlist, regardless of playlist size.

--- spotify_multitool.py
+++ spotify_multitool.py
@@ -60,27 +60,47 @@ def get_user_playlists(token, user_id):
     json_result = json.loads(result.content)["items"]
     return json_result
 
-def get_songs_from_playlist(token, playlist_id):
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks??market=US&fields=fields%3Ditems%28added_by.id%2Ctrack%28name%2Chref%2Calbum%28name%2Chref%29%29%29&limit=5&offset=0"
+def get_songs_from_playlist(token, playlist_id, offset):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks??market=US&fields=fields%3Ditems%28added_by.id%2Ctrack%28name%2Chref%2Calbum%28name%2Chref%29%29%29&limit=50&offset={offset}"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["items"]
     song_total = json.loads(result.content)["total"]
     return json_result, song_total
 
-def print_songs(playlist_json):
-    print("Songs in the Playlist:")
+def print_songs(playlist_json, offset):
+
     for index, song in enumerate(playlist_json, start=1):
         track_name = song["track"]["name"]
-        artist_name = song["track"]["album"]["artists"][0]["name"]
+        #artist_name = song["track"]["album"]["artists"][0]["name"]
+        if "artists" in song["track"]["album"] and song["track"]["album"]["artists"]:
+            artist_name = song["track"]["album"]["artists"][0]["name"]
+        else:
+            artist_name = "Unknown Artist"
     
-        print(f"{index}. {track_name} by {artist_name}")
+        print(f"{index + offset}. {track_name} by {artist_name}")
 
+def parse_input():
+    print("Please enter the link to the playlist you'd like to copy: ")
+    url = input()
 
-playlist_id = "37i9dQZF1DWXRqgorJj26U"
+    start_index = url.find("playlist/") + len("playlist/")
+    end_index = url.find("?", start_index) if "?" in url else len(url)
+
+    playlist_id = url[start_index:end_index]
+    return playlist_id
+
+#playlist_id = "130r8tmjyMRtQt1FRk765o"
 
 token = get_token()
-playlist, total_songs = get_songs_from_playlist(token, playlist_id)
-print("Number of Songs:", total_songs)
-print("\n")
-print_songs(playlist)
+
+playlist_id = parse_input()
+
+counter = 0
+playlist, total_songs = get_songs_from_playlist(token, playlist_id, 0)
+
+print("All songs: ")
+while (counter < total_songs):
+    playlist, total_songs = get_songs_from_playlist(token, playlist_id, counter)
+    print_songs(playlist, counter)
+    counter = counter + 50
